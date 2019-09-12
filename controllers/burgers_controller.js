55d2f9ba45aa394b686797edfa994b34f6060b79
Kyle Wuellner <rockstar81@comcast.net>
2019-09-11 20:02:46 -0500

    adding routing info for burger controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,1 +1,40 @@
+// dependencies
 let express = require("express");
+let burger = require("../models/burger");
+let router = express.Router();
+
+// creating routes and setting up logig
+router.get("/", function (req, res) {
+    burger.selectALL(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"],
+        [req.body.burger_name, req.body.devoured], function (result) {
+            res.json({ id: result.insertID });
+        });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne(
+        {
+            devoured: req.body.devoured
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+
+                return res.status(404).end();
+            }
+        }
+    );
+});
+
+module.exports = router;
