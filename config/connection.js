76bd098fccd9a620b6e6fa9712a8aa87834c744a
Kyle Wuellner <rockstar81@comcast.net>
2019-09-12 22:51:06 -0500

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -8,17 +8,17 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        port: 3306,
         password: "King2012!",
         database: "burgers_db"
     });
 }
-connection.connect(function (err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+// connection.connect(function (err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+connection.connect();
 
 module.exports = connection;
